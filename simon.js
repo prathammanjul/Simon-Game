7aaac05538a73df2ef3dd8b80e9a806f4a0692e8
prathammanjul <prathammanjul91@gmail.com>
2025-09-09 12:09:27 +0530

    Updated Simon Game with responsive design and start button fix

--- simon.js
+++ simon.js
@@ -4,13 +4,13 @@ let userSeq = [];
 let started = false;
 let level = 0;
 
-let btns = ["red" , "yellow" , "green" , "purple"]
+let btns = ["red", "yellow", "green", "purple"]
 
 let h2 = document.querySelector("h2")
 
-document.addEventListener("keypress" , function(){
+document.addEventListener("keypress", function () {
 
-    if(started == false){
+    if (started == false) {
         // console.log("game started")
         started = true;
 
@@ -18,7 +18,6 @@ document.addEventListener("keypress" , function(){
     }
 
 })
-
 
 
 document.querySelector(".start-btn").addEventListener("click", function () {
@@ -31,59 +30,57 @@ document.querySelector(".start-btn").addEventListener("click", function () {
 
 
 
-function gameFlash(btn){
+function gameFlash(btn) {
     btn.classList.add("flash");
-    setTimeout(function(){
+    setTimeout(function () {
         btn.classList.remove("flash");
-    } , 200);
+    }, 200);
 }
 
-function userFlash(btn){
+function userFlash(btn) {
     btn.classList.add("userflash");
-    setTimeout(function(){
+    setTimeout(function () {
         btn.classList.remove("userflash");
-    } , 200);
+    }, 200);
 }
 
-function levelUp(){
+function levelUp() {
     userSeq = [];
     level++;
 
     h2.innerText = `level ${level}`;
 
     //random button choose 
-
     let randIdx = Math.floor(Math.random() * 4);
     let randColor = btns[randIdx];
     let randBtn = document.querySelector(`.${randColor}`)
 
     gameSeq.push(randColor);
-     console.log(gameSeq); 
+    console.log(gameSeq);
 
-   /* console.log(randIdx);
-    console.log(randColor);
-    console.log(randBtn); */
+    /* console.log(randIdx);
+     console.log(randColor);
+     console.log(randBtn); */
 
-    gameFlash(randBtn); 
-} 
+    gameFlash(randBtn);
+}
 
-function checkAns(idx){
+function checkAns(idx) {
     // console.log("curr level : ", level)
 
-    
 
-    if(userSeq[idx] === gameSeq[idx]){
-        if(userSeq.length == gameSeq.length)
-            setTimeout(levelUp , 1000);
+    if (userSeq[idx] === gameSeq[idx]) {
+        if (userSeq.length == gameSeq.length)
+            setTimeout(levelUp, 1000);
     } else {
         h2.innerHTML = `Game Over! Your scrore was <b>${level} </b> <br> Please enter any Key to start Again.`;
         document.querySelector("body").style.backgroundColor = "red";
-        setTimeout(function(){
+        setTimeout(function () {
             document.querySelector("body").style.backgroundColor = "white";
-        } , 150);
+        }, 150);
         reset();
     }
-    
+
 
 }
 
@@ -93,7 +90,7 @@ function checkAns(idx){
 you can check nodelist by using it.
  console.log(allBtn); 
 
- this cant be work without loops because querySelectorAll will give nodelist 
+ this can't be work without loops because querySelectorAll will give nodelist 
  allBtn.addEventListener("click" , function(){
 
     console.log("pressed");
@@ -102,7 +99,7 @@ you can check nodelist by using it.
 
 */
 
-function btnPress(){
+function btnPress() {
     // console.log("Button was pressed");
     let btn = this;
     // console.log(btn);
@@ -114,19 +111,19 @@ function btnPress(){
     userSeq.push(userColor);
     // console.log(userSeq);
 
-    checkAns(userSeq.length-1);
-    
+    checkAns(userSeq.length - 1);
+
 }
 
 // to access all the .btns ,  need to use loops through nodeList 
 let allBtns = document.querySelectorAll(".btn");
 
-for(btn of allBtns){
-    btn.addEventListener("click" , btnPress);
+for (btn of allBtns) {
+    btn.addEventListener("click", btnPress);
 }
 
 
-function reset(){
+function reset() {
     started = false;
     userSeq = [];
     gameSeq = [];
